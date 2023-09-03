bab3104a3f96c86aa3debcf596a15aea1d471301
Muhammadkhizar <khizeraziz786@gmail.com>
2023-09-04 02:28:03 +0500

    work on fixes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  headerTitle = "Repertoire Lists";
+  headerTitle = "Explore Repertoire";
   constructor() { }
 
   ngOnInit() {
